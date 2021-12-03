3717cf1bc6c216976b9abfaf8a8d0ebd6c956dc4
Ruimin Huang <cph-rh206@cphbusiness.dk>
2021-12-03 20:17:37 +0100

    first try of 9g0

--- src/readNWrite.fs
+++ src/readNWrite.fs
@@ -1,4 +1,22 @@
+open System
 //9g0
-///readFile
+///readFile  have to mistakes need to fix
+
+
+
 let readFile (fileName:string) : string option =
+ let reader =
+    try
+     Some (System.IO.File.OpenText fileName)
+    with
+     |_-> None
+
+ if reader.IsSome then
+   Some ((reader.Value).ReadToEnd)
+ else
+   None
+
+printfn "%s" (readFile (readNWrite.fs)).Value
+  
+ 
  
